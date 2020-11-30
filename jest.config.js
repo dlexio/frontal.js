d4c4e0117f6b952b68f7f9c8c6f9c048d7310146
Mohsen Elgendy <5392357+MohsenElgendy@users.noreply.github.com>
2020-11-30 14:54:33 +0200

    feat: initial github test action

--- jest.config.js
+++ jest.config.js
@@ -4,28 +4,15 @@ const path = require('path')
 const corePackages = fs.readdirSync(path.resolve(__dirname, 'packages'))
 
 module.exports = {
-	coverage: true,
 	testEnvironment: 'node',
 	expand: true,
 	forceExit: true,
 	roots: ['<rootDir>/packages', '<rootDir>/test'],
 	coverageDirectory: './coverage',
 	collectCoverageFrom: ['**/packages/*/src/**/*.js'],
-
-	coveragePathIgnorePatterns: [
-		'node_modules/(?!(@nuxt|nuxt))',
-		'packages/webpack/src/config/plugins/vue',
-		'packages/server/src/jsdom',
-	],
-
+	coveragePathIgnorePatterns: [],
 	testPathIgnorePatterns: ['examples/.*'],
-
-	// transform: {
-	//   '^.+\\.js$': 'babel-jest'
-	// },
-
 	moduleFileExtensions: ['js', 'json'],
-
 	moduleNameMapper: {
 		[`@frontal/(${corePackages.join('|')})(/?.*)$`]: '<rootDir>/packages/$1/src/$2',
 	},
