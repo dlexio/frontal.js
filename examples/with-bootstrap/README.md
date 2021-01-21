64c5fef5eb4b9ada36cc091cd6939194590d3006
Mohsen Elgendy <5392357+MohsenElgendy@users.noreply.github.com>
2021-01-21 16:36:01 +0200

    feat: add bootstrap example

---
+++ examples/with-bootstrap/README.md
@@ -0,0 +1,10 @@
+## Bootstrap Example
+
+This example shows how to use Bootstrap and jQuery with Frontal.
+
+## Usage
+
+```
+$ npm i
+$ npm run dev
+```
---
+++ examples/with-bootstrap/frontal.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	bundles: {
+		main: {
+			assets: [
+				'bootstrap',
+				'@assets/style/main.scss',
+				'@assets/js/app.js'
+			],
+			pages: ['**/*.html']
+		}
+	}
+}
