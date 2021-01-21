e048703cb0a01f46094510151e0d2d704d4dff15
Mohsen Elgendy <5392357+MohsenElgendy@users.noreply.github.com>
2021-01-21 16:44:23 +0200

    feat: add bulma example

---
+++ examples/with-bulma/README.md
@@ -0,0 +1,10 @@
+## Bulma Example
+
+This example shows how to use the Bulma CSS framework with Frontal.
+
+## Usage
+
+```
+$ npm i
+$ npm run dev
+```
---
+++ examples/with-bulma/frontal.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	bundles: {
+		main: {
+			assets: [
+				'bulma',
+				'@assets/style/main.scss',
+				'@assets/js/app.js'
+			],
+			pages: ['**/*.html']
+		}
+	}
+}
